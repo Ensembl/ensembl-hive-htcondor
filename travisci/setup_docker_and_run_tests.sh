fd27bb17705056cd9ba5578768dea8cdba6c06ea
Matthieu Muffato <muffato@ebi.ac.uk>
2017-03-19 17:57:00 +0000

    The repo is temporarily under muffato/

--- travisci/setup_docker_and_run_tests.sh
+++ travisci/setup_docker_and_run_tests.sh
@@ -15,7 +15,7 @@ apt-get install -qqy sqlite3 libdbd-sqlite3-perl libdbi-perl libcapture-tiny-per
 # It seems that non-root users cannot execute anything from /home/travis
 # so we copy the whole directory for the condoradmin user
 CONDORADMIN_HOME=/home/condoradmin
-cp -a /home/travis/build/Ensembl/ensembl-hive-htcondor $CONDORADMIN_HOME
+cp -a /home/travis/build/muffato/ensembl-hive-htcondor $CONDORADMIN_HOME
 chown -R condoradmin: $SGEADMIN_HOME/ensembl-hive-htcondor
 sudo --login -u condoradmin $SGEADMIN_HOME/ensembl-hive-htcondor/travisci/run_tests.sh
 
