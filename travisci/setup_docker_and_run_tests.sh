0ead5c11c51798430a613597e5f638d098fe4511
Matthieu Muffato <muffato@ebi.ac.uk>
2017-09-20 15:48:59 +0100

    Simplified the installation procedure thanks to eHive's installation scripts

--- travisci/setup_docker_and_run_tests.sh
+++ travisci/setup_docker_and_run_tests.sh
@@ -8,25 +8,18 @@ set -e
 
 echo "DEBUG: Environment of $0"; env; id; echo "END_DEBUG"
 
-# Install some packages inside the container
-apt-get update
-# Taken from ensembl-hive's Dockerfile
-apt-get install -y cpanminus git build-essential \
-		  sqlite3 libdbd-sqlite3-perl postgresql-client libdbd-pg-perl mysql-client libdbd-mysql-perl libdbi-perl \
-		  libcapture-tiny-perl libdatetime-perl libhtml-parser-perl libjson-perl libproc-daemon-perl \
-		  libtest-exception-perl libtest-simple-perl libtest-warn-perl libtest-warnings-perl libtest-file-contents-perl libtest-perl-critic-perl libgraphviz-perl \
-		  libgetopt-argvfile-perl libchart-gnuplot-perl libbsd-resource-perl
-
 # It seems that non-root users cannot execute anything from /home/travis
 # so we copy the whole directory for the condoradmin user
 CONDORADMIN_HOME=/home/condoradmin
 cp -a /home/travis/build/muffato/ensembl-hive-htcondor $CONDORADMIN_HOME
 CONDOR_CHECKOUT_LOCATION=$CONDORADMIN_HOME/ensembl-hive-htcondor
 chown -R condoradmin: $CONDOR_CHECKOUT_LOCATION
+HIVE_CHECKOUT_LOCATION=$CONDOR_CHECKOUT_LOCATION/ensembl-hive
 
-# Install the missing dependencies (if any)
-cpanm --installdeps --with-recommends $CONDOR_CHECKOUT_LOCATION/ensembl-hive
-cpanm --installdeps --with-recommends $CONDOR_CHECKOUT_LOCATION
+# Install extra packages inside the container
+export DEBIAN_FRONTEND=noninteractive
+$HIVE_CHECKOUT_LOCATION/docker/setup_os.Ubuntu-16.04.sh
+$HIVE_CHECKOUT_LOCATION/docker/setup_cpan.Ubuntu-16.04.sh $HIVE_CHECKOUT_LOCATION $CONDOR_CHECKOUT_LOCATION
 
 sudo --login -u condoradmin $CONDOR_CHECKOUT_LOCATION/travisci/run_tests.sh
 
