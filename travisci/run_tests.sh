cc82f91b6fbc8285bb1d7eca6b11b2fe9adc882f
Matthieu Muffato <muffato@ebi.ac.uk>
2017-11-18 14:27:32 +0000

    Don't need to do anything for ensembl-hive since it is included in the docker image

--- travisci/run_tests.sh
+++ travisci/run_tests.sh
@@ -10,7 +10,6 @@ echo "DEBUG: Environment of $0"; env; id; echo "END_DEBUG"
 
 BUILD_DIR=/home/condoradmin/ensembl-hive-htcondor
 cd $BUILD_DIR
-export EHIVE_ROOT_DIR=$PWD/ensembl-hive
 export PERL5LIB=$EHIVE_ROOT_DIR/modules:$PWD/modules
 export EHIVE_TEST_PIPELINE_URLS='sqlite:///ehive_test_pipeline_db'
 export EHIVE_MEADOW_TO_TEST=HTCondor
--- travisci/setup_docker_and_run_tests.sh
+++ travisci/setup_docker_and_run_tests.sh
@@ -14,11 +14,10 @@ CONDORADMIN_HOME=/home/condoradmin
 cp -a /home/travis/build/Ensembl/ensembl-hive-htcondor $CONDORADMIN_HOME
 CONDOR_CHECKOUT_LOCATION=$CONDORADMIN_HOME/ensembl-hive-htcondor
 chown -R condoradmin: $CONDOR_CHECKOUT_LOCATION
-HIVE_CHECKOUT_LOCATION=$CONDOR_CHECKOUT_LOCATION/ensembl-hive
 
 # Install extra packages inside the container
 export DEBIAN_FRONTEND=noninteractive
-$HIVE_CHECKOUT_LOCATION/docker/setup_cpan.Ubuntu-16.04.sh $CONDOR_CHECKOUT_LOCATION
+$EHIVE_ROOT_DIR/docker/setup_cpan.Ubuntu-16.04.sh $CONDOR_CHECKOUT_LOCATION
 
 sudo --login -u condoradmin $CONDOR_CHECKOUT_LOCATION/travisci/run_tests.sh
 
