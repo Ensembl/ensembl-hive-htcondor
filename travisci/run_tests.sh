469ac6bbefd784f8bc055a0103d8e83a5bd69b75
Matthieu Muffato <muffato@ebi.ac.uk>
2020-09-01 00:24:26 +0100

    Not allowed to write to the mount point

--- travisci/run_tests.sh
+++ travisci/run_tests.sh
@@ -10,11 +10,11 @@ echo "DEBUG: Environment of $0"; env; id; echo "END_DEBUG"
 
 #BUILD_DIR=/home/condoradmin/ensembl-hive-htcondor
 BUILD_DIR=/home/travis/build/Ensembl/ensembl-hive-htcondor
-cd $BUILD_DIR
+#cd $BUILD_DIR
 export EHIVE_ROOT_DIR=/repo/ensembl-hive
-export PERL5LIB=$EHIVE_ROOT_DIR/modules:$PWD/modules
+export PERL5LIB=$EHIVE_ROOT_DIR/modules:$BUILD_DIR/modules
 export EHIVE_TEST_PIPELINE_URLS='sqlite:///ehive_test_pipeline_db'
 export EHIVE_MEADOW_TO_TEST=HTCondor
 
-prove -rv --ext .t --ext .mt t "$EHIVE_ROOT_DIR/t/04.meadow/meadow-longmult.mt"
+prove -rv --ext .t --ext .mt $BUILD_DIR/t "$EHIVE_ROOT_DIR/t/04.meadow/meadow-longmult.mt"
 
